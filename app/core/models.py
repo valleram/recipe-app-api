48c832445faafcd521dde21bf68f2a13890b145d
root <root@localhost.localdomain>
2019-06-11 20:50:00 -0500

    Added custom user model.

--- app/core/models.py
+++ app/core/models.py
@@ -15,6 +15,15 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        """Creates and saves a nwe superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Create user model that supports using email instead of username"""
--- app/core/tests/test_modules.py
+++ app/core/tests/test_modules.py
@@ -18,12 +18,22 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
     	"""Test the email for a new user is normalized"""
-    	email = 'test@LONDONAPPDEV.com'
-    	user = get_user_model().objects.create_user(email, 'test123')
+        email = 'test@LONDONAPPDEV.com'
+        user = get_user_model().objects.create_user(email, 'test123')
 
-    	self.assertEqual(user.email, email.lower())
+        self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@unodefjldjfd.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
