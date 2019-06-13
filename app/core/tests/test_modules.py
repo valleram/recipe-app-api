e13a5f1150c8f836ed63d1b126baa0f93c30f14a
root <root@localhost.localdomain>
2019-06-12 21:09:58 -0500

    Update admin to support admin user model.

--- app/core/tests/test_modules.py
+++ app/core/tests/test_modules.py
@@ -17,7 +17,7 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-    	"""Test the email for a new user is normalized"""
+        """Test the email for a new user is normalized"""
         email = 'test@LONDONAPPDEV.com'
         user = get_user_model().objects.create_user(email, 'test123')
 
